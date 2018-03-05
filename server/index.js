47e40ad0183264cd1a9fcb33ff343db4d8fc066c
fusijie <fusijie@vip.qq.com>
2018-03-05 10:37:12 +0800

    update server logic.

--- server/index.js
+++ server/index.js
@@ -4,24 +4,7 @@ let cors = require("cors");
 let mysql = require("mysql");
 let fs = require("fs");
 
-let data = [];
 let mysql_opt = JSON.parse(fs.readFileSync(__dirname + "/config.json"));
-
-// load data from mysql.
-function load_data() {
-	let connection = mysql.createConnection(mysql_opt);
-	connection.connect();
-	let sql_cmd = 'select * from `CreatorTestData`';
-	connection.query(sql_cmd, function (err, rows, fields) {
-		if (err) {
-			throw err;
-		}
-		for (let i = 0; i < rows.length; i++) {
-			data.push(JSON.parse(rows[i].data));
-		}
-	});
-	connection.end();
-}
 
 // start http server.
 function start_server() {
@@ -39,20 +22,33 @@ function start_server() {
 		let sql_cmd = "insert into CreatorTestData(data) values(?)"
 		connection.query(sql_cmd, JSON.stringify(req.body), function (err, rows, fields) {
 			if (err) {
+				res.sendStatus(500);
 				throw err;
 			}
+			res.send(req.body);
 		});
 		connection.end();
-		data.push(req.body);
-		res.send(req.body);
 	});
 
 	app.post("/get_result", jsonParser, function (req, res) {
-		res.send(data);
+		let data = [];
+		let connection = mysql.createConnection(mysql_opt);
+		connection.connect();
+		let sql_cmd = 'select * from `CreatorTestData`';
+		connection.query(sql_cmd, function (err, rows, fields) {
+			if (err) {
+				res.sendStatus(500);
+				throw err;
+			}
+			for (let i = 0; i < rows.length; i++) {
+				data.push(JSON.parse(rows[i].data));
+			}
+			res.send(data);
+		});
+		connection.end();
 	});
 
-	app.listen(30000, () => console.log("Example app listening on port 30000!"));
+	app.listen(30000, () => console.log("Performance test server app listening on port 30000!"));
 }
 
-load_data();
 start_server();
