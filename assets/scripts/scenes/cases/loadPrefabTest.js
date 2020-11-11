f496aa407de61a39771ada78ea4751ce2896eea0
Jare Guo <jare85@163.com>
2020-11-11 14:54:32 +0800

    preload all assets before Load Prefab Test

--- assets/scripts/scenes/cases/loadPrefabTest.js
+++ assets/scripts/scenes/cases/loadPrefabTest.js
@@ -18,25 +18,26 @@ cc.Class({
     loadRes: function (finish_cb) {
         const gamePrefab = "uiTest/prefab/gamePrefab";
         let _releasePrefab = (prefab) => {
-            let deps = cc.loader.getDependsRecursively(prefab._uuid);
-            cc.loader.release(deps);
+            cc.assetManager.releaseAsset(prefab);
         }
         let _loadPrefab = (count) => {
-            let beforeLoadTime = performance.now();
-            cc.loader.loadRes(gamePrefab, (err, prefab) => {
-                if (!err) {
-                    let afterLoadTime = performance.now();
-                    let gap_time = afterLoadTime - beforeLoadTime;
-                    this.durationTimeArr.push(gap_time);
-                    this.labelLog.string += `loading ${++this.count}: ${Math.round(gap_time)}ms\n`;
-                    count --;
-                    _releasePrefab(prefab);
-                    if (count > 0) {
-                        _loadPrefab(count);
-                    }else {
-                        finish_cb && finish_cb();
+            cc.resources.preload(gamePrefab, (err) => {
+                let beforeLoadTime = performance.now();
+                cc.resources.load(gamePrefab, (err, prefab) => {
+                    if (!err) {
+                        let afterLoadTime = performance.now();
+                        let gap_time = afterLoadTime - beforeLoadTime;
+                        this.durationTimeArr.push(gap_time);
+                        this.labelLog.string += `loading ${++this.count}: ${Math.round(gap_time)}ms\n`;
+                        count --;
+                        _releasePrefab(prefab);
+                        if (count > 0) {
+                            _loadPrefab(count);
+                        }else {
+                            finish_cb && finish_cb();
+                        }
                     }
-                }
+                });
             });
         };
         _loadPrefab(config.SCENE_ARGS.count);
